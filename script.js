c1637515e8103ba80ba2003519dfb746f724f2e5
Chandan1035 <lchandanrao01@gmail.com>
2023-01-07 20:32:12 +0530

    done

--- script.js
+++ script.js
@@ -7,21 +7,38 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  const developers = arr.map(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here
+  const newEmployee = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+  const updatedArray = arr.filter(employee => employee.profession !== "admin");
+  console.log(updatedArray);
 }
 
 function concatenateArray() {
-  //Write your code here
+  const newArray = [
+    {id:5,name:"Raj",age:"28",profession:"intern"},
+    {id:6,name:"Shyam",age:"24",profession:"admin"},
+    {id:7,name:"Raju",age:"25",profession:"developer"}
+  ];
+  const concatenatedArray = arr.concat(newArray);
+  console.log(concatenatedArray);
 }
